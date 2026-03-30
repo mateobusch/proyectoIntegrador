aec7b5c0b8a08c952edf06a9d8a820a80aedbda5
Manuelblanco0 <manuelblanco@udesa.edu.ar>
2026-03-30 17:45:05 -0300

    Buscador.js

---
+++ proyecto-react/src/components/Buscador/Buscador.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+import {withRouter} from "react-router-dom"
+
+class Buscador extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            search: ""
+        }
+    }
+
+    onSubmit(event) {
+        event.preventDefault()
+        console.log("props de buscador", this.props)
+        this.props.history.push("/busqueda/" + this.state.search)
+    }
+
+    guardarBusqueda(event) {
+        this.setState(
+            {search: event.target.value},
+        () => console.log("log desde el setState extendido: ", this.state.search))
+        console.log("El valor en estado es: ", this.state.search)
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={(event)=> this.onSubmit(event)}>
+                    <input onChange={(event)=> this.guardarBusqueda(event)} value={this.state.search}/>
+                    <button type="submit">Buscar</button>
+                </form>
+            </div>
+        )
+    }
+}
+export default Buscador;
