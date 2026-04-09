155cd6afd6944f756e05df20ae724856fd5d77cd
Mateo Busch <mbusch@udesa.edu.ar>
2026-04-09 15:32:39 -0300

    Arranque a armar el movie card

--- proyecto-react/src/components/Header/Header.js
+++ proyecto-react/src/components/Header/Header.js
@@ -1,57 +1,57 @@
-import React, { Component } from 'react'
-import Menu from "../Menu/Menu"
-import {Link} from "react-router-dom"
-import "./styles.css"
+    import React, { Component } from 'react'
+    import Menu from "../Menu/Menu"
+    import {Link} from "react-router-dom"
+    import "./styles.css"
 
- class Header extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            haySesion: false
+    class Header extends Component {
+        constructor(props){
+            super(props)
+            this.state = {
+                haySesion: false
+            }
         }
-    }
-    componentDidMount(){
-        let usuarioLogeado = localStorage.getItem("usuario");
-        if (usuarioLogeado !== null){
-            this.setState({
-                haySesion: true
-            })
+        componentDidMount(){
+            let usuarioLogeado = localStorage.getItem("usuario");
+            if (usuarioLogeado !== null){
+                this.setState({
+                    haySesion: true
+                })
+            }
         }
-    }
-    render() {
-    
-    return (
+        render() {
         
-         <nav>
-            <ul className="nav nav-tabs my-4">
-                <li className   ="nav-item">
-                    <Link to= "/home">Home</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to= "/peliculas">Peliculas</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to= "/series">Series</Link>
-                </li>
-                {this.state.haySesion ?
-                <li className="nav-item">
-                    <Link className="nav-link" to= "/favoritos">Favoritos</Link>
-                </li>
-                : null}
-                {!this.state.haySesion ? 
-                <li className="nav-item ml-auto">
-                    <Link className="nav-link" to= "/registro">Registro</Link>
-                </li>
-                : null}
-                {!this.state.haySesion ?
-                <li className="nav-item">
-                   <Link className="nav-link" to= "/login">Login</Link>
-                </li>
-                : null}
-            </ul>
-        </nav>
-    )
-  }
-}
+        return (
+            
+            <nav>
+                <ul className="nav nav-tabs my-4">
+                    <li className   ="nav-item">
+                        <Link to= "/home">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to= "/peliculas">Peliculas</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to= "/series">Series</Link>
+                    </li>
+                    {this.state.haySesion ?
+                    <li className="nav-item">
+                        <Link className="nav-link" to= "/favoritos">Favoritos</Link>
+                    </li>
+                    : null}
+                    {!this.state.haySesion ? 
+                    <li className="nav-item ml-auto">
+                        <Link className="nav-link" to= "/registro">Registro</Link>
+                    </li>
+                    : null}
+                    {!this.state.haySesion ?
+                    <li className="nav-item">
+                    <Link className="nav-link" to= "/login">Login</Link>
+                    </li>
+                    : null}
+                </ul>
+            </nav>
+        )
+    }
+    }
 
-export default Header
+    export default Header
