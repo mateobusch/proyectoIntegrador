db67c5a5a8c257a8c5ddf0b9e67e94689dac80b2
Manuelblanco0 <manuelblanco@udesa.edu.ar>
2026-04-13 15:38:41 -0300

    cambio

--- proyecto-react/src/Screens/Peliculas/Peliculas.js
+++ proyecto-react/src/Screens/Peliculas/Peliculas.js
@@ -1,6 +1,4 @@
-
 import React, { Component } from 'react';
-import MovieCard from '../../components/MovieCard/MovieCard'
 
 class Peliculas extends Component {
     constructor(props) {
@@ -18,12 +16,12 @@ class Peliculas extends Component {
     }
 
     traerPeliculas() {
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=4606c83ccea5f9b56977aeac833b6148&page=${this.state.proximaPagina}`)
+        fetch("https://api.themoviedb.org/3/movie/popular?api_key=4606c83ccea5f9b56977aeac833b6148&page=${this.state.proximaPagina}")
             .then(response => response.json())
             .then(data => {
                 this.setState({
-                    peliculas: data.results,
-                    peliculasFiltradas: data.results,
+                    peliculas: this.state.peliculas(data.results),
+                    peliculasFiltradas: this.state.peliculas(data.results),
                     proximaPagina: this.state.proximaPagina + 1
                 });
             })
@@ -31,8 +29,8 @@ class Peliculas extends Component {
     }
     render() {
         return (
-           <p>Peliculas</p>
-        )
+           
+        );
     }
 }
 
