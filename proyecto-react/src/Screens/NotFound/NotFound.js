63411889d2ae60e72db56d4ef79cc41c4a6d1915
brunocoppola <bcoppola@udesa.edu.ar>
2026-04-19 19:41:33 -0300

    arreglando el index.html y las clases del notfound

--- proyecto-react/src/Screens/NotFound/NotFound.js
+++ proyecto-react/src/Screens/NotFound/NotFound.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 class NotFound extends Component {
     render() {
         return (
-            <div className="container NotFound">
-                <h1 className="div404">404</h1>
-                <h2 className="alert alert-warning">Contenido Inexistente</h2>
+            <div className="my-5 text-center">
+                <h1 className="alert alert-danger">404</h1>
+                <h2 className="alert alert-danger">Contenido Inexistente</h2>
                 <p className="texto404">Lo sentimos, la página que estás buscando no existe en UdeSA Movies.</p>
                 <img 
                     src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGIycWp6Zmt6Zmt6Zmt6Zmt6Zmt6Zmt6Zmt6Zmt6Zmt6Zmt6JmU9Zw/h2P01cZLZzMK4/giphy.gif" 
